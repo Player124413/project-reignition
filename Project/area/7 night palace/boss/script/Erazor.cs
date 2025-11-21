2b9ad7823b533a5ed21b42d4a9f794e520c9531b
Kuma <kumapauze@gmail.com>
2025-11-20 22:13:46 -0500

    Update Animations

--- Project/area/7 night palace/boss/script/Erazor.cs
+++ Project/area/7 night palace/boss/script/Erazor.cs
@@ -4,5 +4,31 @@ namespace Project.Gameplay.Bosses;
 
 public partial class Erazor : Node3D
 {
+	[Export] private AnimationTree animationTree;
+	[Export] private PathFollow3D pathFollower;
 
+
+	[Export] private string attackPattern;
+	private int remainingHealth;
+
+	/// <summary> The preferred distance when attacking the player </summary>
+	private readonly float AttackDistance = 10f;
+
+	public override void _Ready()
+	{
+		animationTree.Active = true; // Activate animation trees
+
+		StageSettings.Instance.Respawned += Respawn;
+		StageSettings.Instance.LevelStarted += StartIntroduction;
+	}
+
+	public void Respawn()
+	{
+
+	}
+
+	public void StartIntroduction()
+	{
+
+	}
 }
