7819a9ed02c58954448c86cf190925f350aa1b30
Kuma <kumapauze@gmail.com>
2026-01-19 14:32:24 -0500

    Add Crouch to Backflip transition

--- Project/object/player/resource/script/states/CrouchState.cs
+++ Project/object/player/resource/script/states/CrouchState.cs
@@ -8,6 +8,7 @@ public partial class CrouchState : PlayerState
 	[Export] private PlayerState idleState;
 	[Export] private PlayerState runState;
 	[Export] private PlayerState jumpState;
+	[Export] private PlayerState backflipState;
 	[Export] private PlayerState fallState;
 	[Export] private PlayerState homingAttackState;
 
@@ -63,6 +64,10 @@ public partial class CrouchState : PlayerState
 		else if (Player.Controller.IsJumpBufferActive)
 		{
 			Player.Controller.ResetJumpBuffer();
+
+			if (Player.IsBackflipInputValid())
+				return backflipState;
+
 			return jumpState;
 		}
 		else if (!Input.IsActionPressed("button_action") && !Player.Animator.IsCrouchTransitionActive)
