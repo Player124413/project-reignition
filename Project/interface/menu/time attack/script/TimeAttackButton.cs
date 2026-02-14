2adfd3a6ba1b099c846e8bcb55a1bbc5b4c3ffe9
Arctiiq <codythnc@gmail.com>
2026-02-14 01:49:42 -0500

    Implement new art assets for Time Attack's main menu

---
+++ Project/interface/menu/time attack/script/TimeAttackButton.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+namespace Project.Interface.Menus;
+
+public partial class TimeAttackButton : Menu
+{
+	[Export] private String text;
+	[Export] public String description { get; private set; }
+	[Export] private Label label;
+	[Export] public Texture2D image { get; private set; }
+
+	public void SetText() => label.Text = text;
+	public void ShowButton() => animator.Play("show");
+	public void HideButton() => animator.Play("hide");
+	public void SelectButton() => animator.Play("select");
+	public void DeselectButton() => animator.Play("deselect");
+
+	public void ConfirmButton() => animator.Play("confirm");
+}
