1f87235b45682bc0688f5dacc1684b7235d5fc41
Kuma <kumapauze@gmail.com>
2026-03-10 20:39:43 -0400

    Implemented basic connection menu

---
+++ Project/party/script/core/network_manager.gd
@@ -0,0 +1,160 @@
+### Manages creating servers and adding clients.
+extends Node
+
+## Emitted when the host successfully connects.
+signal host_connected
+## Emitted when a client successfully connects.
+signal client_connected
+
+## Determines whether this device is acting as a host or a client.
+var is_hosting_game : bool
+## The address to use for connections.
+var address : String = "tomfol.io"
+## The port to use for connections.
+var port = 8890
+## Represents the room id that players can give to each other.
+var room_id : String
+## Tracks whether the game is connected online or not.
+var is_online : bool
+## Determines whether to attempt NAT Punchthrough or not. Enable this when exporting the project.
+var is_nat_enabled : bool = false
+
+func start_network_signals():
+	print("Noray network ready!")
+	if is_hosting_game:
+		setup_host_noray_connection_signals()
+	else:
+		setup_client_noray_connection_signals()
+
+## Entry point for Host.
+func create_server_peer():
+	print("Creating Noray server peer")
+	await _register_with_noray()
+	_start_noray_host()
+
+## Entry point for Client.
+func create_client_peer():
+	print("create Noray client peer")
+	await _register_with_noray()
+	setup_client_enet_connection_signals()
+	Noray.connect_nat(room_id)
+
+func _register_with_noray():
+	print("Register with Noray hosted at: %s" % address)
+	var err = OK
+	
+	# connect to Noray
+	err = await Noray.connect_to_host(address, port)
+	if err != OK:
+		print("Failed to connect to Noray for registration at %s:%s" % [address, port, err])
+		return err
+		
+	# Register host
+	Noray.register_host()
+	await Noray.on_pid
+	
+	# Capture room_id to display on host-peer for sharing with others
+	print("Noray oid/gameId: %s" % Noray.oid)
+	room_id = Noray.oid
+	
+	# Register remove address
+	err = await Noray.register_remote()
+	if err != OK:
+		print("Failed to register remote %s" % err)
+		return err
+	
+	print("Finished Noray registration")
+
+## Starts the Noray host.
+func _start_noray_host():
+	print("Starting Noray host")
+	var noray_network_peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+	var err = noray_network_peer.create_server(Noray.local_port)
+	multiplayer.multiplayer_peer = noray_network_peer
+	if err != OK:
+		print("Failed to listen on port %s with error: %s" % [Noray.local_port, err])
+		return
+	
+	is_online = true
+	host_connected.emit()
+	print("SERVER: Started listening on port %s" % Noray.local_port)
+
+## Server method for handling client connections.
+func _handle_noray_client_connect(_address: String, _port: int) -> Error:
+	print("Noray host handle connect: %s:%s" % [_address, _port])
+	var peer = multiplayer.multiplayer_peer as ENetMultiplayerPeer
+	var err = await PacketHandshake.over_enet(peer.host, _address, _port)
+	if err != OK:
+		print("Noray packet handshake failed %s" % err)
+		return err
+	
+	print("SERVER: Client %s connected to the server" % peer.get_unique_id())
+	client_connected.emit()
+	return OK
+
+## Client method for attempting a NAT Punchthrough.
+func _handle_nat_connect(_address: String, _port: int) -> Error:
+	if !is_nat_enabled: # NAT was disabled
+		print("NAT is disabled. Using relay instead.")
+		Noray.connect_relay(room_id)
+		return OK
+	print("Attempting to connect client via NAT: %s:%s" % [_address, _port])
+	var err = await _handle_connect(_address, _port)
+	if err != OK:
+		print("NAT connection failed from client, trying Relay instead...")
+		Noray.connect_relay(room_id)
+		return OK
+	else:
+		print("NAT punchthrough successful!")
+	return err
+
+## Client method for attempting a relay connection.
+func _handle_relay_connect(_address: String, _port: int) -> Error:
+	print("Attempting to connect client via Relay: %s:%s" % [_address, _port])
+	return await _handle_connect(_address, _port)
+
+## Client method for handling a successful connection.
+func _handle_connect(_address: String, _port: int) -> Error:
+	print("Client handle connect to %s:%s, Noray.localport: %s" % [_address, _port, Noray.local_port])
+	
+	# Do a UDP handshake
+	var udp = PacketPeerUDP.new()
+	udp.bind(Noray.local_port)
+	udp.set_dest_address(_address, _port)
+	var err = await PacketHandshake.over_packet_peer(udp, 8)
+	udp.close()
+	if err != OK:
+		print("Client packet handshake failed %s" % err)
+		return err
+	
+	# Connect to host
+	var peer = ENetMultiplayerPeer.new()
+	err = peer.create_client(_address, _port, 0, 0, 0, Noray.local_port)
+	if err != OK:
+		print("Create client failed %s" % err)
+		return err
+		
+	multiplayer.multiplayer_peer = peer
+	is_online = true
+	client_connected.emit()
+	print("CLIENT: Peer %s has connected." % multiplayer.get_unique_id())
+	return OK
+
+## Noray host connections.
+func setup_host_noray_connection_signals():
+	Noray.on_connect_nat.connect(_handle_noray_client_connect)
+	Noray.on_connect_relay.connect(_handle_noray_client_connect)
+
+## Noray client connections.
+func setup_client_noray_connection_signals():
+	Noray.on_connect_nat.connect(_handle_nat_connect)
+	Noray.on_connect_relay.connect(_handle_relay_connect)
+
+## Client ENet signals 
+func setup_client_enet_connection_signals():
+	multiplayer.server_disconnected.connect(_noray_server_disconnected)
+
+## Called when the client disconnects from the server.
+func _noray_server_disconnected():
+	is_online = false
+	print("Disconnected from Noray server.")
--- Project/party/script/interface/online_menu.gd
+++ Project/party/script/interface/online_menu.gd
@@ -3,6 +3,8 @@ extends Menu
 
 ## Nodes for selecting an option from the online menu.
 @export var online_nodes : Array[Control]
+@export var address_edit : LineEdit
+@export var room_edit : LineEdit
 @export var selection_label : SyncedLabel
 @export var transition_label : SyncedLabel
 @export var cursor : Control
@@ -11,6 +13,9 @@ extends Menu
 var is_connection_menu_active : bool = true
 ## Tracks whether the player is trying to be the host, join, or play offline.
 var connection_mode_selection : int
+
+func get_max_vertical_selection() -> int:
+	return online_nodes.size() - 2
 
 ## Localization keys for connection selection options.
 var connection_values : Array[String] = [
@@ -47,11 +52,23 @@ func update_connection_selection() -> void:
 ## Updates selection for [address, port, connect] menu.
 func update_online_selection() -> void:
 	var previous_selection : Vector2i = current_selection
-	current_selection.y = clamp(current_selection.y + input_axis.y, 0, online_nodes.size() - 2)
-	if current_selection.y == online_nodes.size() - 2:
+	current_selection.y = clamp(current_selection.y + input_axis.y, 0, get_max_vertical_selection())
+	if current_selection.y == get_max_vertical_selection():
 		current_selection.x = clamp(current_selection.x + input_axis.x, 0, 1)
 	else:
 		current_selection.x = 0
+	
+	room_edit.release_focus()
+	address_edit.release_focus()
+	if online_nodes[current_selection.y] == address_edit:
+		address_edit.grab_focus()
+		address_edit.edit()
+	elif online_nodes[current_selection.y] == room_edit and room_edit.editable:
+		room_edit.grab_focus()
+		room_edit.edit()
+	else:
+		grab_focus()
+	
 	if previous_selection != current_selection:
 		start_selection_timer()
 
@@ -67,7 +84,32 @@ func confirm() -> void:
 		animator.play("show-online-menu")
 		is_connection_menu_active = false
 		return
-	## TODO Process connection requests
+	if room_edit.has_focus() || address_edit.has_focus():
+		return
+	# Process connection requests
+	if current_selection.y == get_max_vertical_selection():
+		if current_selection.x == 1:
+			connect_noray()
+
+## Starts a connection to the Noray server.
+func connect_noray() -> void:
+	NetworkManager.is_hosting_game = connection_mode_selection == 1
+	NetworkManager.start_network_signals()
+	if !address_edit.text.is_empty():
+		NetworkManager.address = address_edit.text
+	if !room_edit.text.is_empty():
+		NetworkManager.room_id = room_edit.text
+	
+	# Connect to server
+	if NetworkManager.is_hosting_game:
+		NetworkManager.host_connected.connect(_on_host_connected)
+		NetworkManager.create_server_peer()
+	else:
+		NetworkManager.create_client_peer()
+
+## Called when the host connects to Noray. Updates the GameID and allows players to copy room data.
+func _on_host_connected() -> void:
+	room_edit.text = NetworkManager.room_id
 
 func cancel() -> void:
 	if is_connection_menu_active:
@@ -76,6 +118,8 @@ func cancel() -> void:
 		animator.play("return-to-main-menu")
 		return
 	
+	if room_edit.has_focus() || address_edit.has_focus():
+		return
 	# Return to connection mode selection
 	is_connection_menu_active = true
 	animator.play("hide-online-menu")
