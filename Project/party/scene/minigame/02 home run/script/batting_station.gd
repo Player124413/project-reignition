cb9aed31e4f34fd1d6e3006b82c109a4af59e631
Kuma <kumapauze@gmail.com>
2026-04-04 21:59:11 -0400

    Implement Batting and CPU Behavior

---
+++ Project/party/scene/minigame/02 home run/script/batting_station.gd
@@ -0,0 +1,229 @@
+extends Node3D
+
+signal demo_finished
+
+@export var player_index : int
+@export var character_animator : CharacterAnimator
+
+@export var spawn_position : Node3D
+@export var bat_attachment : BoneAttachment3D
+@export var catapult_animator : AnimationPlayer
+@export var ball : Node3D
+@export var ball_target : Node3D
+
+@export_group("CPU Settings")
+## Curves that are sampled to determine cpu's offset amount
+@export var cpu_difficulty_curves : Array[Curve]
+
+## The ratio at which CPU player will swing. re-calculated every time a ball is pitched.
+var cpu_swing_ratio : float
+## Tracks whether the cpu can swing or not. Prevents multiple random swings.
+var can_cpu_swing : bool
+## Tracks whether the player is swinging or not.
+var is_swinging : bool
+## Ratio from [0, 1] that determines the ball's position along its current trajectory.
+var travel_ratio : float
+## List of possible pitch timings.
+const PITCH_LENGTHS : Array[float] = [1.0, 2.0, 1.5]
+
+## How close the ball has to be to the bat to count as a hit.
+const HIT_WINDOW : float = 12.0;
+const HOME_RUN_WINDOW : float = 8.0;
+const IN_FIELD_ANGLE : float = PI * 0.25;
+
+## How far the ball should go during a home run.
+const HOME_RUN_DISTANCE : float = 1200.0
+## How far the ball should go during an in-field.
+const IN_FIELD_DISTANCE : float = 500.0
+## How long the ball should travel when hit.
+const HIT_TRAVEL_LENGTH : float = 2.0
+## How high the ball should fly when hit.
+const HIT_HEIGHT : float = 100.0
+
+## Distance at which a "Strike" occurs and a new ball is thrown.
+const STRIKE_DISTANCE : float = 100.0
+
+## Multiplier for the catapult animation, because it's too slow by default.
+const CATAPULT_ANIMATION_MULTIPLIER : float = 1.5
+## Multiplier for the swing animation, because it feels sluggish otherwise.
+const SWING_ANIMATION_MULTIPLIER : float = 1.8
+## Windup length used for CPU controlled players to calculate when to swing.
+const SWING_ANIMATION_WINDUP : float = 0.3333333;
+## Maximum amount of swing variance in CPU controlled players.
+const CPU_MAX_VARIANCE : float = 0.1;
+
+## The speed at which the ball is currently travelling at.
+var ball_speed : float
+## The position the ball was hit at.
+var ball_start_position : Vector3
+## The position the ball will end at.
+var ball_end_position : Vector3
+
+var ball_state : BALL_STATES
+enum BALL_STATES {
+	LOAD,
+	PITCH,
+	HIT
+}
+
+## Total number of balls to send over the course of the mini-game.
+const BAT_COUNT : int = 10
+
+func _ready() -> void:
+	if NetworkManager.is_online:
+		# TODO Set up authority
+		pass
+	
+	if player_index != -1:
+		# Instance Player Model
+		character_animator = MinigameManager.instance.load_character_model(player_index)
+		spawn_position.add_child(character_animator)
+		character_animator.play_animation(get_anim_prefix() + "wait")
+		
+		# TODO Check if this player index is actually being used
+		set_physics_process(false)
+		MinigameManager.instance.gameplay_started.connect(Callable.create(self, "activate"))
+	
+	bat_attachment.reparent(character_animator.skeleton)
+	character_animator.connect("animation_event", Callable.create(self, "process_animation_event"))
+
+func activate() -> void:
+	set_physics_process(true)
+	pitch_ball() # Start pitching balls
+
+func _physics_process(_delta: float) -> void:
+	if !is_multiplayer_authority():
+		# TODO Sync with other users
+		return
+	
+	process_swing()
+	process_ball()
+
+func process_swing() -> void:
+	if is_swinging:
+		return
+	
+	if player_index == -1 || PartyManager.get_player_data(player_index).is_cpu_player(): # CPU Behaviour
+		process_cpu()
+	elif Input.is_action_just_pressed("button_primary1"):
+		start_swing()
+
+## Simply swings at the right time. The swing timing is set in calculate_swing_ratio().
+func process_cpu() -> void:
+	if can_cpu_swing && ball_state == BALL_STATES.PITCH && cpu_swing_ratio < travel_ratio:
+		can_cpu_swing = false
+		start_swing()
+
+func start_swing() -> void:
+	character_animator.play_minigame_animation(get_anim_prefix() + "swing", 0.1, SWING_ANIMATION_MULTIPLIER)
+	is_swinging = true
+
+## Handles ball movement.
+func process_ball() -> void:
+	if ball_state == BALL_STATES.LOAD:
+		return
+	
+	travel_ratio = move_toward(travel_ratio, 1.0, ball_speed * get_physics_process_delta_time())
+	ball.global_position = sample_hit_position(travel_ratio)
+	if !is_equal_approx(travel_ratio, 1.0) || catapult_animator.is_playing():
+		return
+	
+	if player_index == -1: # Finished the demo!
+		demo_finished.emit()
+		print("Emitting demo finished!")
+		# Stop processing
+		set_process(false)
+		set_physics_process(false)
+	else:
+		pitch_ball()
+
+func sample_hit_position(ratio : float) -> Vector3:
+	var ball_position : Vector3 = ball_start_position.lerp(ball_end_position, ratio)
+	if ball_state == BALL_STATES.HIT: # Add vertical height
+		ball_position.y += sin(PI * ratio) * HIT_HEIGHT
+	return ball_position
+
+func get_anim_prefix() -> String:
+	return "02-" if player_index == -1 else "%s/" % MinigameManager.ANIMATION_LIBRARY_PREFIX
+
+## Handle animation events from the character's animation controller
+func process_animation_event(info : int) -> void:
+	if info == 0: # Return to idle
+		is_swinging = false
+		character_animator.play_minigame_animation(get_anim_prefix() + "wait", 0.1)
+	elif info == 1: # Strike the ball
+		check_hit()
+
+func check_hit() -> void:
+	if ball_state != BALL_STATES.PITCH: # Ball is not travelling
+		return
+	var distance : float = ball.global_position.distance_to(ball_target.global_position)
+	if distance < HIT_WINDOW:
+		var travel_direction : Vector3 = (ball_end_position - ball_start_position).normalized()
+		var hit_direction : int = sign(travel_direction.dot(ball_target.global_position - ball.global_position))
+		hit_ball(distance, hit_direction)
+
+func hit_ball(distance : float, direction : int, _network_time : float = 0.0) -> void:
+	print(distance)
+	var angle : float = calculate_hit_angle(distance) * direction
+	var target_distance : float = HOME_RUN_DISTANCE if distance < HOME_RUN_WINDOW else IN_FIELD_DISTANCE
+	
+	# Change state
+	ball_state = BALL_STATES.HIT
+	travel_ratio = 0.0 # TODO Change this based on network_time
+	ball_speed = calculate_travel_speed(HIT_TRAVEL_LENGTH)
+	ball_start_position = ball.global_position
+	ball_end_position = ball_target.global_position
+	ball_end_position += Vector3.FORWARD.rotated(Vector3.UP, angle) * target_distance
+
+func calculate_hit_angle(hit_distance : float) -> float:
+	return IN_FIELD_ANGLE * (hit_distance / HIT_WINDOW)
+
+## Starts a pitch.
+func pitch_ball() -> void:
+	catapult_animator.play("catapult", -1, CATAPULT_ANIMATION_MULTIPLIER)
+	catapult_animator.seek(0.0)
+	ball_state = BALL_STATES.LOAD
+	ball.visible = false
+
+## Called from the catapult animation when the ball spawns.
+func load_ball() -> void:
+	ball.top_level = false
+	ball.transform = Transform3D.IDENTITY
+	ball.reset_physics_interpolation()
+	ball.visible = true
+
+## Called from the catapult animation when the ball launches.
+func launch_ball() -> void:
+	ball.top_level = true
+	ball_state = BALL_STATES.PITCH
+	ball_start_position = ball.global_position
+	ball_end_position = ball_target.global_position
+	ball_end_position += (ball_end_position - ball_start_position).normalized() * STRIKE_DISTANCE
+	ball_speed = calculate_travel_speed(PITCH_LENGTHS[0]) # TODO Use a randomly generated index that is synced across the network
+	travel_ratio = 0.0
+	
+	if is_multiplayer_authority():
+		calculate_swing_ratio()
+
+## Calculates the swing ratio for CPUs.
+func calculate_swing_ratio() -> void:
+	var total_distance : float = ball_start_position.distance_to(ball_end_position)
+	var target_distance : float = ball_start_position.distance_to(ball_target.global_position) # This is the target contact ratio
+	var swing_offset : float = SWING_ANIMATION_WINDUP / SWING_ANIMATION_MULTIPLIER + get_physics_process_delta_time() * 2.0
+	if player_index != -1:
+		# Add randomness based on CPU difficulty
+		swing_offset += calculate_cpu_difficulty_offset()
+	
+	target_distance -= total_distance * ball_speed * swing_offset # Offset by windup length (relative to ratio)
+	cpu_swing_ratio = target_distance / total_distance
+	can_cpu_swing = true # Allow the cpu to swing
+
+func calculate_cpu_difficulty_offset() -> float:
+	var difficulty : int = PartyManager.get_player_data(player_index).cpu_difficulty
+	var offset : float = cpu_difficulty_curves[difficulty].sample(randf()) * CPU_MAX_VARIANCE
+	print("Offset %s by %s." % [player_index, offset])
+	return offset
+
+func calculate_travel_speed(travel_length : float) -> float:
+	return 1.0 / travel_length
--- Project/party/script/character animation/character_animator.gd
+++ Project/party/script/character animation/character_animator.gd
@@ -1,6 +1,9 @@
 ### Interface for handling animations of a party character.
 class_name CharacterAnimator extends Node3D
 
+signal animation_event(info : int)
+
+@export var skeleton : Skeleton3D
 @export var animator : AnimationPlayer
 
 ## Emitted after the select animation finishes. Emitted from aniamtion.
@@ -9,12 +12,28 @@ signal select_finished
 
 # TODO Add support for more complex character animations
 
+## Emits a signal from an animation. Use this to send data to the generic minigame controller.
+func emit_animation_event(info : int) -> void:
+	emit_signal("animation_event", info)
+
 ## Plays a specific animation on the animator.
 func play_animation(anim : StringName) -> void:
 	if animator == null || !animator.has_animation(anim):
 		return
 	animator.play(anim)
 	animator.seek(0.0, true)
+
+## Plays a specific mini-game animation on the animator.
+func play_minigame_animation(anim : StringName, blend : float = 0.0, speed : float = 1.0) -> void:
+	if animator == null || !animator.has_animation(anim):
+		return
+	
+	if anim == animator.assigned_animation:
+		animator.seek(0.0, true)
+	animator.play("%s" % anim, blend, speed)
+
+func queue_minigame_animation(anim : StringName) -> void:
+	animator.queue("%s" % anim)
 
 ## Loads a given animation library to the animator.
 func load_animation_library(library_name : String, library : AnimationLibrary) -> void:
