ab04b15ab564c08f3d6a0595e4648dae8ae830b6
Arctiiq <codythnc@gmail.com>
2026-04-21 01:14:38 -0400

    Mod Manager loading

--- Project/core/ModManager.cs
+++ Project/core/ModManager.cs
@@ -8,6 +8,7 @@ namespace Project.Core;
 public partial class ModManager : Node
 {
 
+	public static ModManager Instance;
 	public LevelDataResource[] ModdedLevels { get; private set; }
 	private readonly string CustomLevelPath = "user://custom/levels/";
 
@@ -18,18 +19,42 @@ public partial class ModManager : Node
 
 	private void SetUpMods()
 	{
+
+		if (!DirAccess.DirExistsAbsolute(CustomLevelPath))
+			DirAccess.MakeDirRecursiveAbsolute(CustomLevelPath);
+
 		DirAccess dir = DirAccess.Open(CustomLevelPath);
 
 		if (dir == null)
 			return;
 
-		foreach (string file in dir.GetFiles())
+		foreach (string file in dir.GetFiles()) //Iterates through all level mods
 		{
-			GD.Print("LOADING MOD: " + file);
-			if (file.GetExtension().Equals(".pck"))
+			GD.Print("LOADING MOD: " + CustomLevelPath + file);
+			if (file.GetExtension().Equals("pck"))
 			{
-
+				var success = ProjectSettings.LoadResourcePack(CustomLevelPath + file); //Loads the pck
+				if (!success)
+					continue; //If we failed, keep looking for pcks
+				else
+					GD.Print("LOADING MOD Succeeded");
 			}
+			GD.Print("FINISHED LOADING MOD: " + file);
 		}
+
+		DirAccess modDir = DirAccess.Open("res://mods/levels/");
+
+		foreach (string mod in modDir.GetDirectories())//Gets all the directories that were added
+		{
+			DirAccess levelDir = DirAccess.Open("res://mods/levels/" + mod); //Access the specific mod directory
+
+			foreach (string level in levelDir.GetFiles()) //Look through the files for the stage resource
+			{
+				GD.Print("Level File: " + level);
+			}
+			GD.Print(mod);
+		}
+
+
 	}
 }
