431821166e45e0df6dfb8bd6ef5f34d2bdd22ce2
Kuma <kumapauze@gmail.com>
2026-05-06 21:28:40 -0400

    Pre-create objects

--- Project/party/scene/minigame/05 parasol ball/script/parasol_ball_spawner.gd
+++ Project/party/scene/minigame/05 parasol ball/script/parasol_ball_spawner.gd
@@ -1,12 +1,10 @@
 extends Node3D
 
-@export var parasol_ball_scene : PackedScene
 @export var spawn_bounds : Vector2
-@export var total_ball_count : int = 45
 @export var total_spawn_length : float = 30
 @export var total_bonus_ball_count : int = 12
 
-var ball_pool : Array
+var ball_pool : Array[Area3D]
 var current_ball_index : int
 var deactivated_ball_count : int
 var spawn_timer : float
@@ -19,27 +17,26 @@ func _ready() -> void:
 	if !NetworkManager.is_hosting_game:
 		return
 	
-	_spawn_interval = total_spawn_length / total_ball_count
+	_spawn_interval = total_spawn_length / ball_pool.size()
 	MinigameManager.instance.gameplay_started.connect(Callable(self,"start_spawning"))
 
 func on_ball_deactivated() -> void:
 	if !NetworkManager.is_hosting_game:
 		return
 	deactivated_ball_count += 1
-	if deactivated_ball_count >= total_ball_count:
+	if deactivated_ball_count >= ball_pool.size():
 		MinigameManager.instance.request_autoplay_results()
 
 func start_spawning() -> void:
 	set_physics_process(true)
 
 func initialize_ball_pool() -> void:
-	ball_pool.resize(total_ball_count)
-	for i in total_ball_count:
-		var new_ball : Node3D = parasol_ball_scene.instantiate() as Node3D
-		new_ball.deactivated.connect(Callable(self, "on_ball_deactivated"), CONNECT_ONE_SHOT)
-		new_ball.call_deferred("initialize", false)
-		add_child(new_ball)
-		ball_pool[i] = new_ball
+	for i in get_child_count():
+		ball_pool.append(get_child(i))
+	
+	for ball in ball_pool:
+		ball.deactivated.connect(Callable(self, "on_ball_deactivated"), CONNECT_ONE_SHOT)
+		ball.call_deferred("initialize", false)
 
 func _physics_process(delta: float) -> void:
 	if !NetworkManager.is_hosting_game:
