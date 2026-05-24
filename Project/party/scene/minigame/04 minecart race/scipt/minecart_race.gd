19880606772f497007762ab13f827fd760166a06
Arctiiq <codythnc@gmail.com>
2026-05-24 10:50:00 -0400

    added speed variables to minecart

--- Project/party/scene/minigame/04 minecart race/scipt/minecart_race.gd
+++ Project/party/scene/minigame/04 minecart race/scipt/minecart_race.gd
@@ -5,8 +5,6 @@ extends PartyGameCharacterSpawner
 @export var camera: Camera3D;
 @export var minecart_animator: AnimationPlayer
 
-## Is the player currently pumping the lever?
-var is_pumping: bool
 var lever_state: LEVER_STATES
 enum LEVER_STATES {
 	UP,
@@ -14,14 +12,19 @@ enum LEVER_STATES {
 }
 
 ## How fast should the minecart go without pumping
-const MINECART_DEFAULT_SPEED: float = 10.0
+const MINECART_MIN_SPEED: float = 10.0
 ## How fast should the minecart go with pumping
-const MINECART_PUMP_SPEED: float = 15.0
-## How many frames can the player hold the lever before reverting to the default pump speed
-const MINECART_PUMP_HOLD_TIME: float = 20
+const MINECART_MAX_SPEED: float = 35.0
+## How much speed should be added each pump
+const MINECART_ADDITIVE_SPEED: float = 5.0
+## How much speed should be added on an incline
+const MINECART_INCLINE_SPEED: float = 15.0
+var speed
+
 
 func on_spawn_finished() -> void:
 	lever_state = LEVER_STATES.UP
+	speed = MINECART_MIN_SPEED
 
 	
 func _physics_process(_delta: float) -> void:
@@ -30,21 +33,20 @@ func _physics_process(_delta: float) -> void:
 
 ##If the lever is up, pump down, and vice versa
 func process_pump() -> void:
-	if is_pumping:
-		return
-	
 	if minecart_animator.is_playing():
 		return
 
 	if Input.is_action_just_pressed("button_primary%s" % get_input_suffix()) && lever_state == LEVER_STATES.UP:
 		start_player_pump_down()
+		return
 	
-	if Input.is_action_just_pressed("button_secondary%s" % get_input_suffix()) && lever_state == LEVER_STATES.DOWN:
+	if Input.is_action_just_pressed("button_primary%s" % get_input_suffix()) && lever_state == LEVER_STATES.DOWN:
 		start_player_pump_up()
 
 ## Pushes the minecart lever down
 func start_player_pump_down() -> void:
 	minecart_animator.play("down")
+	speed += MINECART_ADDITIVE_SPEED
 	return
 
 ## Pushes the minecart lever up
