54ecd924441c24475a5dac33c4e44960655500a7
Arctiiq <codythnc@gmail.com>
2026-06-03 12:59:57 -0400

    The player can now get hurt by other hammers

--- Project/party/scene/minigame/11 giant stake/script/player_controller.gd
+++ Project/party/scene/minigame/11 giant stake/script/player_controller.gd
@@ -4,51 +4,56 @@ extends PartyGameCharacterMover
 @export var hand_attachment: BoneAttachment3D
 @export var stake_spawner: StakeSpawner
 @export var hammer_collision: CollisionShape3D
+@export var player_hitbox: CollisionShape3D
 
 func on_spawn_finished() -> void:
 	super ()
 	hand_attachment.reparent(character_animator.skeleton)
-	swing_state = SWING_STATE.IDLE
+	_state = STATE.IDLE
 
 func on_minigame_finished() -> void:
 	super ()
 	hand_attachment.visible = false
 
-var swing_state: SWING_STATE
-enum SWING_STATE {
+var _state: STATE
+enum STATE {
 	IDLE,
-	SWING
+	SWING,
+	DAMAGE
 }
 
 
 func process_rotation(target_angle: float) -> void:
-	if swing_state == SWING_STATE.IDLE:
+	if _state == STATE.IDLE:
 		super (target_angle)
 
 func process_speed() -> void:
-	if swing_state == SWING_STATE.IDLE:
+	if _state == STATE.IDLE || _state == STATE.DAMAGE:
 		super ()
 		return
 	
 	_move_speed = move_toward(_move_speed, 0, brake_friction * get_physics_process_delta_time())
 
 func process_animation() -> void:
-	if swing_state == SWING_STATE.IDLE:
+	if _state == STATE.IDLE:
 		super ()
 
 const ANIM_SWING_FINISH: int = 0
 const ANIM_SWING_START: int = 1
+const ANIM_DAMAGE_START: int = 2
+const ANIM_DAMAGE_FINISH: int = 3
 
 func process_animation_event(event: int) -> void:
 	if event == ANIM_SWING_FINISH: # Finished swinging
-		swing_state = SWING_STATE.IDLE
-		hammer_collision.disabled = true
+		_state = STATE.IDLE
 	elif event == ANIM_SWING_START:
-		swing_state = SWING_STATE.SWING
+		_state = STATE.SWING
+	elif event == ANIM_DAMAGE_FINISH:
+		_state = STATE.IDLE
 	
 
 func process_inputs() -> void:
-	if !is_cpu() && swing_state == SWING_STATE.IDLE:
+	if !is_cpu() && _state == STATE.IDLE:
 		if Input.is_action_just_pressed("button_primary%s" % get_input_suffix()):
 			rpc("start_swing", NetworkTimeSynchronizer.get_time())
 		if Input.is_action_just_pressed("button_secondary%s" % get_input_suffix()):
@@ -58,13 +63,31 @@ func process_inputs() -> void:
 @rpc("any_peer", "call_local", "reliable")
 func start_swing(tick: float) -> void:
 	hammer_collision.disabled = false
-	swing_state = SWING_STATE.SWING
+	_state = STATE.SWING
 	var target_anim: StringName
 	target_anim = get_anim_prefix() + "hammer-down"
 	character_animator.rpc("play_minigame_animation", target_anim, 0, 1, 0, tick)
 
+@rpc("any_peer", "call_local", "reliable")
+func take_damage() -> void:
+	if _state == STATE.DAMAGE: # Makes it so we can't be combo'd
+		return
+	character_animator.play_minigame_animation(get_anim_prefix() + "hurt")
+	_state = STATE.DAMAGE
 
+##The hitbox for the hammer
 func _on_area_3d_area_entered(area: Area3D) -> void:
-	if area.is_in_group("enemy"):
+	if _state != STATE.SWING:
+		return
+	
+	if !is_multiplayer_authority():
+		return
+	
+	if area == player_hitbox:
+		return
+
+	if area.is_in_group("enemy") || area.is_in_group("player"):
 		hammer_collision.disabled = true
-	pass # Replace with function body.
+
+	if area.is_in_group("player"):
+		area.get_parent().rpc("take_damage")
