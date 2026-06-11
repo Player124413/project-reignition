1b22b831194a38c6ab27359e3cb5495263b3495e
Arctiiq <codythnc@gmail.com>
2026-06-11 19:24:21 -0400

    pick-up implementation

--- Project/party/scene/minigame/16 treasure box/script/chest.gd
+++ Project/party/scene/minigame/16 treasure box/script/chest.gd
@@ -1,13 +1,14 @@
-class_name Chest extends Node
+class_name TreasureChest extends Node
 
-@export var num_coins: int
+@export var num_coins: int = 0
 @export var animator: AnimationPlayer
 @export var rigidbody: RigidBody3D
 @export var smoke: GPUParticles3D
-
+@export var debug_label: Label3D
 
 func _on_rigid_body_3d_body_entered(body: Node) -> void:
 	if body.is_in_group("floor"):
+		debug_label.text = str(num_coins)
 		animator.play("hit_floor")
 		smoke.emitting = true
 		
