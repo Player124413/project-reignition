ea852b50bdd6914b2c0070d4c23c029427c4b4fe
Kuma <kumapauze@gmail.com>
2026-06-15 13:38:15 -0400

    Import Environment

---
+++ Project/party/scene/minigame/17 pitch black/script/picture.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+@export var amy_materials : Array[StandardMaterial3D]
+@export var blaze_materials : Array[StandardMaterial3D]
+@export var cream_materials : Array[StandardMaterial3D]
+@export var knuckles_materials : Array[StandardMaterial3D]
+@export var shadow_materials : Array[StandardMaterial3D]
+@export var silver_materials : Array[StandardMaterial3D]
+@export var sonic_materials : Array[StandardMaterial3D]
+@export var tails_materials : Array[StandardMaterial3D]
+@export var demo_wrong : StandardMaterial3D
+@export var image_mesh : MeshInstance3D
