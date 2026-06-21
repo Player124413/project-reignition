1b4b28a612916102163e98c5d4db67adbf826c6a
Kuma <kumapauze@gmail.com>
2026-06-21 13:49:35 -0400

    Fix Gyro influencing mouse and keyboard controls

--- Project/resource/script/SkillRing.cs
+++ Project/resource/script/SkillRing.cs
@@ -28,9 +28,9 @@ public class SkillRing
 	public int GetSkillCountByElement(SkillResource.SkillElement element) => SkillCountByElement[(int)element];
 
 	/// <summary> Returns whether the autorun skill is equipped, or the player has Gyro enabled. </summary>
-	public bool IsAutorunActive => IsSkillEquipped(SkillKey.Autorun) || SaveManager.Config.isGyroEnabled;
+	public bool IsAutorunActive => IsSkillEquipped(SkillKey.Autorun) || (SaveManager.Config.isGyroEnabled && Runtime.Instance.IsUsingController);
 	/// <summary> Returns whether the freeroam skill is equipped and the player doesn't have Gyro enabled. </summary>
-	public bool IsFreeRoamActive => IsSkillEquipped(SkillKey.FreeRoam) && !SaveManager.Config.isGyroEnabled;
+	public bool IsFreeRoamActive => IsSkillEquipped(SkillKey.FreeRoam) && !(SaveManager.Config.isGyroEnabled && Runtime.Instance.IsUsingController);
 
 	/// <summary> Calculates how many skill points the player has based on their level. </summary>
 	public static int CalculateSkillPointsByLevel(int level)
