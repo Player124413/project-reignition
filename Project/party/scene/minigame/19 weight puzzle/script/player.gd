afde5360f4fe16f8a04be4fc4572a2eb9b0d2b3f
Arctiiq <codythnc@gmail.com>
2026-07-15 12:03:41 -0400

    Attempted demo intro

--- Project/party/scene/minigame/19 weight puzzle/script/player.gd
+++ Project/party/scene/minigame/19 weight puzzle/script/player.gd
@@ -7,6 +7,8 @@ extends PartyGameCharacterSpawner
 @export var hand_attachment: BoneAttachment3D
 @export var debug_label: Label3D
 
+var zeroout_hands: bool = false
+
 ##The current number of coins in the bucket
 var num_coins: int
 
@@ -16,15 +18,47 @@ const COINS_TO_REACH: int = 50
 func on_spawn_finished() -> void:
 	super()
 
-	character_animator.play_animation("%s/19-lift-wait" % MinigameManager.ANIMATION_LIBRARY_PREFIX, true)
+	#character_animator.play_animation("%s/19-lift-wait" % MinigameManager.ANIMATION_LIBRARY_PREFIX, true)
+	character_animator.play_animation("wait")
 	hand_attachment.reparent(character_animator.skeleton)
+	demo_sequence()
 
 func _process(delta: float) -> void:
 	debug_label.text = str(num_coins)
 
+	if zeroout_hands:
+		zero_hands()
+	
+		
 func catch_chest() -> void:
-	coin_box.reparent(hand_attachment)
+	set_zero(true)
 	character_animator.play_animation("%s/19-catch" % MinigameManager.ANIMATION_LIBRARY_PREFIX, true)
+	coin_box.reparent(hand_attachment)
 
 func throw_chest() -> void:
 	character_animator.play_animation("%s/19-return" % MinigameManager.ANIMATION_LIBRARY_PREFIX, true)
+
+func unparent() -> void:
+	set_zero(false)
+	coin_box.reparent(weight_platform2)
+
+func demo_sequence() -> void:
+	if player_index != 0:
+		return
+	
+	weight_animator.play("weight_toss")
+	await get_tree().create_timer(3.5).timeout
+	character_animator.play_animation("%s/19-slant1" % MinigameManager.ANIMATION_LIBRARY_PREFIX, true)
+	##TODO: Emit coin particles when box is tilted
+	await get_tree().create_timer(5).timeout
+	weight_animator.play("weight_return")
+	await get_tree().create_timer(1.1).timeout
+	weight_animator.play("check_pos_4")
+	##TODO: Put in circle animation from pitch black
+
+func zero_hands() -> void:
+	coin_box.position = Vector3.ZERO
+	coin_box.rotation = Vector3.ZERO
+
+func set_zero(zero: bool) -> void:
+	zeroout_hands = zero
