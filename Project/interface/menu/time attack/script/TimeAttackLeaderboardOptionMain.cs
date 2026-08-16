e2163ea3683557fe74ba32a2c76789689be7a8a2
Arctiiq <codythnc@gmail.com>
2026-08-15 23:50:40 -0400

    Added default times for leaderboard

--- Project/interface/menu/time attack/script/TimeAttackLeaderboardOptionMain.cs
+++ Project/interface/menu/time attack/script/TimeAttackLeaderboardOptionMain.cs
@@ -6,11 +6,9 @@ public partial class TimeAttackLeaderboardOptionMain : Menu
 {
 	[Export] private Label placement;
 	[Export] private Label time;
-	[Export] private Label newLabel;
 
 	public void SetPlacement(int place) => placement.Text = place.ToString() + ".";
 	public void SetTime(float thisTime) => time.Text = ExtensionMethods.FormatTime(thisTime);
 
 	public void TimeVisible(bool isVisible) => time.Visible = isVisible;
-	public void NewVisible(bool isVisible) => newLabel.Visible = isVisible;
 }
